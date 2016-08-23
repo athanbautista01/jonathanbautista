0186d3ea2364acc729cbe23a1a82b51876b69e9d
Jonathan Bautista <jonathanbautista1997@gmail.com>
2016-08-23 22:31:35 +0800

    google analytics added
    updated with google analytics settings

--- include/header.php
+++ include/header.php
@@ -25,6 +25,16 @@
   <script src="custom/js/custom.js"></script>
 </head>
 <body>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+    ga('create', 'UA-76173555-3', 'auto');
+    ga('send', 'pageview');
+  
+  </script>
   <header>
     <nav class="navbar-default navbar-fixed-top" role="navigation">
       <!-- Brand and toggle get grouped for better mobile display -->
