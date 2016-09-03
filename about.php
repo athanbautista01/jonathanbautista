bed5d79690bfa6517f58c6452c9c5a1e47e5a286
Jonathan Bautista <jonathanbautista1997@gmail.com>
2016-09-03 18:02:21 +0800

    added Facebook Follow Plugin setting

--- about.php
+++ about.php
@@ -19,6 +19,10 @@ include("include/header.php");
 </header>
 <div class="container top">
 <h1>About</h1>
+
+<!-- Facebook Follow Plugin -->
+<iframe src="//www.facebook.com/plugins/follow?href=https%3A%2F%2Fwww.facebook.com%2Fathan.bautista01&amp;layout=standard&amp;show_faces=true&amp;colorscheme=light&amp;width=450&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>
+
 </div>
 
 <?php
